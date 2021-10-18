85b3403abf0b9a94d084757dc8436fd04b4b0f21
Manuel Sosa <mr.sosa@uniandes.edu.co>
2021-10-18 13:15:04 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+	
+	private int numMarcos;
+	
+	private int numPaginas;
+	
+	private int numRef;
+	
+	private Integer[] TP;
+		
+	private Integer[] ram;
+	
+	
+	public Main() {
+		
+	}
+	
+	public static void main(String [] args) {
+		
+	}
+}
